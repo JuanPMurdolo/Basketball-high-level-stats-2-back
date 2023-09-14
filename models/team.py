e14ee167e9f81651b504a0695e2e59998a36170d
unknown <jpmurdolo@gmail.com>
2023-09-14 19:50:24 -0300

    Agregar estadisticas

--- models/team.py
+++ models/team.py
@@ -4,12 +4,12 @@ class Team(db.Model):
     __tablename__ = "teams"
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     name = db.Column(db.String(64), nullable=False)
-    city = db.Column(db.String(64), nullable=False)
-    abbreviation = db.Column(db.String(3), nullable=False)
-    conference = db.Column(db.String(32), nullable=False)
-    division = db.Column(db.String(32), nullable=False)
-    court = db.Column(db.String(128), nullable=False)
-    alternative_court = db.Column(db.String(128), nullable=True)
+    city = db.Column(db.String(64), nullable=True)
+    logo = db.Column(db.String(128), nullable=True)
+    abbreviation = db.Column(db.String(3), nullable=True)
+    conference = db.Column(db.String(32), nullable=True)
+    division = db.Column(db.String(32), nullable=True)
+    court = db.Column(db.String(128), nullable=True)
     players = db.relationship("Player", backref="team", lazy=True)
     stats = db.relationship("Stats", secondary="team_stats", back_populates="teams", lazy="dynamic")
 
--- resources/team.py
+++ resources/team.py
@@ -3,11 +3,13 @@ import uuid
 from flask import request
 from flask.views import MethodView
 from flask_smorest import abort, Blueprint
-from schemas import PlayerSchema, TeamSchema
+from schemas import PlayerSchema, StatsSchema, TeamSchema
 from sqlalchemy.exc import SQLAlchemyError
 from db import db
 from models.team import Team as TeamModel
 from models.player import Player as PlayerModel
+from models.stats import Stats as StatsModel
+from models.game import Game as GameModel
 
 blp = Blueprint(
     "teams", __name__, url_prefix="/teams", description="Operations on teams"
@@ -88,3 +90,50 @@ class Team(MethodView):
             db.session.rollback()
             abort(500, message="Internal server error")
         return team
+    
+@blp.route("/<int:team_id>/game/<int:game_id>/local/stats")
+class Team(MethodView):
+    @blp.arguments(StatsSchema)
+    @blp.response(200, StatsSchema)
+    def patch(self, stats_data, team_id, game_id):
+        team = TeamModel.query.get_or_404(team_id)
+        if team is None:
+            abort(404, message="Team doesn't exist")
+        stats = StatsModel(**stats_data)
+        game = GameModel.query.get_or_404(game_id)
+        if game is None:
+            abort(404, message="Game doesn't exist")
+        team.stats.append(stats)
+        game.local_team_stats.append(stats)
+        try:
+            db.session.add(game)
+            db.session.add(stats)
+            db.session.add(team)
+            db.session.commit()
+        except SQLAlchemyError as e:
+            db.session.rollback()
+            abort(500, message="Internal server error")
+
+@blp.route("/<int:team_id>/game/<int:game_id>/visitor/stats")
+class Team(MethodView):
+    @blp.arguments(StatsSchema)
+    @blp.response(200, StatsSchema)
+    def patch(self, stats_data, team_id, game_id):
+        team = TeamModel.query.get_or_404(team_id)
+        if team is None:
+            abort(404, message="Team doesn't exist")
+        stats = StatsModel(**stats_data)
+        game = GameModel.query.get_or_404(game_id)
+        if game is None:
+            abort(404, message="Game doesn't exist")
+        team.stats.append(stats)
+        game.visitor_team_stats.append(stats)
+        try:
+            db.session.add(game)
+            db.session.add(stats)
+            db.session.add(team)
+            db.session.commit()
+        except SQLAlchemyError as e:
+            db.session.rollback()
+            abort(500, message="Internal server error")
+
