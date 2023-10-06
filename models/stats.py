370b56d0d4c301a4142ad67f795a5dc5dead5086
unknown <jpmurdolo@gmail.com>
2023-10-06 12:10:52 -0300

    New Schema PlayerTotalStats

--- models/stats.py
+++ models/stats.py
@@ -7,9 +7,33 @@ class StatsModel(db.Model):
     __tablename__ = "stats"
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     player_id = db.Column(db.Integer, db.ForeignKey("players.id"), nullable=False)
-    field_goal_attempts = db.Column(db.Integer, nullable=False)
-    field_goal_made = db.Column(db.Integer, nullable=False)
+    #Two point
+    two_point_attempts = Column(Integer, nullable=False, default=0)
+    two_point_made = Column(Integer, nullable=False, default=0)
+    #Three point
+    three_point_attempts = Column(Integer, nullable=False, default=0)
+    three_point_made = Column(Integer, nullable=False, default=0)
     
+    @hybrid_property
+    def field_goal_attempts(self):
+        return self.two_point_attempts + self.three_point_attempts
+
+    @hybrid_property
+    def field_goal_made(self):
+        return self.two_point_made + self.three_point_made
+    
+    @hybrid_property
+    def two_point_percentage(self):
+        if self.two_point_attempts == 0:
+            return 0.0
+        return (self.two_point_made / self.two_point_attempts) * 100.0
+    
+    @hybrid_property
+    def three_point_percentage(self):
+        if self.three_point_attempts == 0:
+            return 0.0
+        return (self.three_point_made / self.three_point_attempts) * 100.0
+
     @hybrid_property
     def field_goal_percentage(self):
         if self.field_goal_attempts == 0:
--- schemas.py
+++ schemas.py
@@ -11,6 +11,12 @@ class PlainStatsSchema(Schema):
     field_goal_attempts = fields.Int(required=True)
     field_goal_made = fields.Int(required=True)
     field_goal_percentage = fields.Float()
+    two_point_attempts = fields.Int(required=True)
+    two_point_made = fields.Int(required=True)
+    three_point_attempts = fields.Int(required=True)
+    three_point_made = fields.Int(required=True)
+    two_point_percentage = fields.Float()
+    three_point_percentage = fields.Float()
 
 class PlayerSchema(PlainPlayerSchema):
     stats = fields.Nested(PlainStatsSchema, many=True)
@@ -19,3 +25,11 @@ class PlayerStatsSchema(Schema):
     id = fields.Int(dump_only=True)
     player_id = fields.Nested(PlainPlayerSchema, dump_only=True)
     stats_id = fields.Nested(PlainStatsSchema, dump_only=True)
+
+class PlayerTotalStats(PlainStatsSchema):
+    id = fields.Int(dump_only=True)
+
+class PlayerTotalStatsSchema(Schema):
+    id = fields.Int(dump_only=True)
+    player_id = fields.Nested(PlainPlayerSchema, dump_only=True)
+    totalStats_id = fields.Nested(PlayerTotalStats, dump_only=True)
