48dc144c46cfa9ac467ed7b87a1e9e6d06a5fcfa
Juan P Murdolo <jpmurdolo@gmail.com>
2024-12-31 20:24:31 -0300

    Pending relationships

--- schemas.py
+++ schemas.py
@@ -17,6 +17,27 @@ class PlainCoachSchema(Schema):
     age = fields.Integer(required=False, validate=validate.Range(min=0, max=100))
     teams = fields.String(required=False)
 
+class PlainStatsSchema(Schema):
+    points = fields.Integer(required=True, validate=validate.Range(min=0))
+    defensive_rebounds = fields.Integer(required=True, validate=validate.Range(min=0))
+    offensive_rebounds = fields.Integer(required=True, validate=validate.Range(min=0))
+    rebounds = fields.Integer(required=True, validate=validate.Range(min=0))
+    assists = fields.Integer(required=True, validate=validate.Range(min=0))
+    steals = fields.Integer(required=True, validate=validate.Range(min=0))
+    blocks = fields.Integer(required=True, validate=validate.Range(min=0))
+    turnovers = fields.Integer(required=True, validate=validate.Range(min=0))
+    fouls = fields.Integer(required=True, validate=validate.Range(min=0))
+    minutes = fields.Integer(required=True, validate=validate.Range(min=0))
+    field_goals_made = fields.Integer(required=True, validate=validate.Range(min=0))
+    field_goals_attempted = fields.Integer(required=True, validate=validate.Range(min=0))
+    two_points_made = fields.Integer(required=True, validate=validate.Range(min=0))
+    two_points_attempted = fields.Integer(required=True, validate=validate.Range(min=0))
+    three_points_made = fields.Integer(required=True, validate=validate.Range(min=0))
+    three_points_attempted = fields.Integer(required=True, validate=validate.Range(min=0))
+    free_throws_made = fields.Integer(required=True, validate=validate.Range(min=0))
+    free_throws_attempted = fields.Integer(required=True, validate=validate.Range(min=0))
+
+
 class TeamSchema(PlainTeamSchema):
     id = fields.Integer(required=True)
 
@@ -24,4 +45,7 @@ class PlayerSchema(PlainPlayerSchema):
     id = fields.Integer(required=True)
 
 class CoachSchema(PlainCoachSchema):
+    id = fields.Integer(required=True)
+
+class StatsSchema(PlainStatsSchema):
     id = fields.Integer(required=True)
